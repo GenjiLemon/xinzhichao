9d7c3003da8e08d9d748767487cfc76b57bdc067
lemon <601749122@qq.com>
2020-08-10 20:44:45 +0800

    ceshi

--- test/Program.cs
+++ test/Program.cs
@@ -19,7 +19,8 @@ namespace test
         {
 
             NestOfHeart.IBLL.IUserManager UserMgr = new NestOfHeart.BLL.UserManager();
-            UserMgr.Register("fay", "fay123", 2);
+            //UserMgr.Register("fay", "fay123", 2);
+			UserMgr.Register("fayfadsga34q325", "fa412341235weqy123", 2);
             UserMgr.Register("cjl", "cjl123", 1);
 
         }
